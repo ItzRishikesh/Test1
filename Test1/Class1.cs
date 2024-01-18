f2c1d27a7b4ceed245c05d5104bb224b43cf9367
RishikeshPawar <rishikeshpawar5050@gmail.com>
2024-01-18 16:26:59 +0530

    Add project files.

---
+++ Test1/Class1.cs
@@ -0,0 +1,134 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    public class BinarySearchPrograms
+    {
+        //Simple Binary Search 
+        //It require Sorted array
+       public static int BinarySearch(int[] arr,int target)
+        {
+            int left = 0;
+            int right = arr.Length-1;
+            while (left<right)
+            {
+                int mid=left+(right-left)/2;
+                if (arr[mid]==target)
+                    return mid;
+                if (arr[mid] < target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return -1;
+
+        }
+
+        /// <summary>
+        /// This method will return greatest number which is greater than or equal to target.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="target"></param>
+        /// <returns>Integer</returns>
+        public static int SealingOfTarget(int[] arr, int target)
+        {
+            if (target < arr[arr.Length - 1])
+            {
+                return -1;
+            }
+            int left = 0;
+            int right = arr.Length-1;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (arr[mid] == target)
+                    return mid;
+                if (arr[mid] < target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return right;
+        }
+        /// <summary>
+        /// This method will return and integer value from given array which is smallest than or equal to target.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="target"></param>
+        /// <returns>Integer</returns>
+        public static int FlooreOfTarget(int[] arr, int target)
+        {
+            if (target>arr[arr.Length-1])
+            {
+                return -1;
+            }
+            int start= 0;
+            int end=arr.Length-1;
+            while (start<=end)
+            {
+                int mid=start+(end-start)/2;
+                if (arr[mid]==target)
+                    return mid;
+                if (target < arr[mid])
+                {
+                    end = mid - 1;
+
+                }else if(target > arr[mid])
+                {
+                    start = mid + 1;
+                }else
+                    return mid;
+            }
+            return end;
+        }
+
+        public static char SmallestLettar()
+        {
+
+        }
+
+    }
+
+    public static class Sorting
+    {
+        public static void ArrayQuickSort(int[]arr,int low,int hi)
+        {
+            if (low>=hi)
+            {
+                return;
+            }
+            int s = low;
+            int e=hi;
+            int m = low + (e-s) / 2;
+            int pivot = arr[m];
+            while (s<=e)
+            {
+                while (arr[s]<pivot)
+                {
+                    s++;
+                }
+                while (arr[e]>pivot)
+                {
+                    e--;
+                }
+                if (s<=e)
+                {
+                    int temp = arr[s];
+                    arr[s] = arr[e];
+                    arr[e] = temp;
+                    s++;
+                    e--;
+                }
+            }
+            ArrayQuickSort(arr, low, e);
+            ArrayQuickSort(arr, s, hi);
+
+        }
+        
+    }
+}
---
+++ Test1/Program.cs
@@ -0,0 +1,93 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using Test1;
+
+string CheckPal(string palName)
+{
+
+    string rev=palName.ToCharArray().Reverse().ToString();
+    string newString = rev + palName;
+    char[] chars = newString.ToCharArray();
+    char[] chars2 = Array.Empty<char>();
+    chars=chars.Reverse().ToArray();
+    if (chars.ToString() == chars2.ToString())
+        return "Palindrome";
+    return "Not Palendrome";
+}
+void FIndSecondLarg(int[] arr)
+{
+
+    Array.Sort(arr, (x, y) => y.CompareTo(x));
+ //   List<int> list = new List<int>();   
+	//for (int i = 0; i < arr.Length; i++)
+	//{
+ //       for (int j = 1; j < arr.Length; j++)
+ //       {
+ //           if (arr[i] > arr[j])
+ //           {
+ //               list.Add(arr[i]);
+ //           }
+ //       }
+        
+ //   }
+    Console.WriteLine(arr[1]);
+   // return list[1];
+}
+
+
+ static bool robot(string moves )
+{
+    int x=0, y=0;
+
+    foreach (char item in moves)
+    {
+        switch (item)
+        {
+            case 'U': y++; break;
+            case 'D': y--;break;
+            case 'L':x--; break;
+            case 'R':x++; break;
+        }
+    }
+    Console.WriteLine(x == 0 && y == 0);
+     
+     return x == 0 && y==0;
+
+}
+
+static int BinarySearch(int[] array, int target)
+{
+    int left = 0;
+    int right = array.Length - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        // Check if the target is present at the middle
+        if (array[mid] == target)
+            return mid;
+
+        // If the target is greater, ignore the left half
+        if (array[mid] < target)
+            left = mid + 1;
+
+        // If the target is smaller, ignore the right half
+        else
+            right = mid - 1;
+    }
+
+    // If the target is not present in the array
+    return -1;
+}
+CheckPal("dhfusidhfusid");
+int[] srr = { 1, 2, 5, 8, 89 };
+
+int[] arr = { 7,5,1000,738,6666,79879,25346,1000 };
+Console.WriteLine(BinarySearchPrograms.SealingOfTarget(srr, 7));
+Console.WriteLine(BinarySearchPrograms.FlooreOfTarget(srr, 7));
+FIndSecondLarg(srr);
+
+Console.ReadKey();
+
